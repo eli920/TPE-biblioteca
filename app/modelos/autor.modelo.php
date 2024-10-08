1d347982ab4a77eed47e291e298a023c2147b6b0
Claribel Rodriguez <rclaribel568@gmail.com>
2024-10-08 13:18:16 -0300

    Se agrega config.php y el Auto deploy

--- app/modelos/autor.modelo.php
+++ app/modelos/autor.modelo.php
@@ -1,9 +1,25 @@
 <?php
+require_once 'config.php';
+
 class AutorModelo {
     private $bd;
 
     public function __construct() {
-        $this->bd = new PDO('mysql:host=localhost;dbname=tpe-biblioteca;charset=utf8', 'root', '');
+        $this->bd = new PDO(
+            "mysql:host=".MYSQL_HOST .
+            ";dbname=".MYSQL_DB.";charset=utf8", 
+            MYSQL_USER, MYSQL_PASS);
+            $this->_desplegar();
+    }
+
+    private function _desplegar() {
+        $consulta = $this->bd->query('SHOW TABLES');
+        $tablas = $consulta->fetchAll();
+        if(count($tablas) == 0) {
+            $sql = <<<END
+            END;
+            $this->bd->query($sql);
+        }
     }
 
     public function obtenerAutores() {
