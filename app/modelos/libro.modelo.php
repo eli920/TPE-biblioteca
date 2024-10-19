c302ee3ed709139a7bada79e431ee9c335397e01
Claribel Rodriguez <rclaribel568@gmail.com>
2024-10-19 18:58:52 -0300

    Se agrega modelo padre. Modificaciones en auto deploy

--- app/modelos/libro.modelo.php
+++ app/modelos/libro.modelo.php
@@ -1,30 +1,10 @@
 <?php
-// require_once 'config.php';
+require_once 'app/modelos/base.modelo.php';
 
-class LibroModelo{
-    private $bd;
-
+class LibroModelo extends ModeloBase{
     public function __construct() {
-        $this->bd = new PDO('mysql:host=localhost;dbname=tpe-biblioteca;charset=utf8', 'root', '');
-     }
-  
-
-    // public function __construct() {
-    //     $this->bd = new PDO(
-    //         "mysql:host=".MYSQL_HOST . ";dbname=".MYSQL_DB.";charset=utf8",  MYSQL_USER, MYSQL_PASS);
-    //     $this->_desplegar();
-    // }
-
-    // private function _desplegar() {
-    //     $consulta = $this->bd->query('SHOW TABLES');
-    //     $tablas = $consulta->fetchAll();
-    //     if(count($tablas) == 0) {
-    //         $sql = <<<END
-    //         END;
-    //         $this->bd->query($sql);
-    //     }
-    // }
-    //
+        parent::__construct();
+    }
 
     public function obtenerLibros() {
 
